914ec78dd79523b17c1a26388bfc8af43d0927dd
DarisX <henrychen9314@gmail.com>
2024-05-06 18:01:08 +0800

    sim server to learn client cache

--- server/main.js
+++ server/main.js
@@ -1,15 +1,27 @@
 const express = require("express")
 const path = require("path")
-const dayjs = require("dayjs")
+// const dayjs = require("dayjs")
 const app = express();
+
+// 增加3000ms延迟，模拟服务器响应时间
+app.use((req, res, next) => {
+	setTimeout(next, 3000)
+})
 
 app.use(express.static(path.join(__dirname, 'source'), {
 	setHeaders: (res, path) => {
 		if (path.includes("private")) {
-			res.set("Cache-Control", "private,max-age=30, must-revalidate");
-			// res.set("Expires", dayjs().add(10, 's'))
-		} else {
+			res.set("Cache-Control", "private, max-age=600");
+		} else if (path.includes("public")) {
 			res.set('Cache-Control', 'public, max-age=600');
+		} else if (path.includes("immutable")) {
+			res.set('Cache-Control', 'immutable, max-age=60');
+		} else if (path.includes('no-store')) {
+			res.set('Cache-Control', 'no-store')
+		} else if (path.includes("no-cache")) {
+			res.set('Cache-Control', 'no-cache, max-age=600')
+		} else if (path.includes("must-revalidate")) {
+			res.set('Cache-Control', 'must-revalidate, max-age=60')
 		}
 	}
 }))
